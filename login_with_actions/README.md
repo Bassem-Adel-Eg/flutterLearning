e0ee4d3a89a3f6b47f2e1c8fb8cdf20b301c4da3
unknown <Bassema@silverkeytech.com>
2019-04-02 14:10:51 +0200

    Adding Actions To Login Form

---
+++ login_with_actions/README.md
@@ -0,0 +1,16 @@
+# login_with_actions
+
+A new Flutter project.
+
+## Getting Started
+
+This project is using Form and TextFormField Widgets
+
+with validation of password bigger than 8 charaters
+
+show a SnackBar when Logins is Wrong 
+
+Show anew page when the logins is right
+
+Deleting the password when returning from a navigation 
+![](image.jpg)
---
+++ login_with_actions/lib/login.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>{
+  final scafoldKey = new GlobalKey<ScaffoldState>();
+  final formKey = new GlobalKey<FormState>();
+
+  final FocusNode passwordfocusNode = new FocusNode();
+  final passwordController = new TextEditingController();
+  String _username;
+  String _password;
+
+  void login(){
+    final loginform = formKey.currentState;
+    if(loginform.validate())
+    {
+      loginform.save();
+      if(_username == 'Bassem' && _password == '1234'){
+        passwordController.text = "";
+        Navigator.push(context, MaterialPageRoute(builder: (context) => new LoggedInSuccessfully()));  
+      }
+      else{
+        final SnackBar  d = new SnackBar(content: Text('Sorry wrong logins'),);
+        scafoldKey.currentState.showSnackBar(d);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scafoldKey,
+      appBar: AppBar(
+        title: Text('Welcome'),
+      ),
+      body: Center(
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Container(
+                  decoration: new BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius:
+                          new BorderRadius.all(Radius.circular(10.0))),
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  margin: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextFormField(
+                    onSaved: (val)=> _username = val,
+                    textInputAction: TextInputAction.next,
+                    onFieldSubmitted: (text) => FocusScope.of(context).requestFocus(passwordfocusNode),
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.person,
+                          color: Colors.white70,
+                        ),
+                        border: InputBorder.none,
+                        labelText: 'User Name Or email address',
+                        labelStyle: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Container(
+                  decoration: new BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius:
+                          new BorderRadius.all(Radius.circular(10.0))),
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  margin: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: TextFormField( 
+                    onFieldSubmitted: (text){login();},
+                    focusNode: passwordfocusNode,
+                    obscureText: true,
+                    onSaved: (val)=> _password = val,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.lock_open,
+                          color: Colors.white70,
+                        ),
+                        border: InputBorder.none,
+                        labelText: 'Password',
+                        labelStyle: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: RaisedButton(
+                  child: Text('Login'),
+                  onPressed:login,
+                )
+              ),
+            ],
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+class LoggedInSuccessfully extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome',)
+        ),
+      body: Center(
+        child: Text("welcome To Our Play"),
+        ),
+    );
+  }
+}
